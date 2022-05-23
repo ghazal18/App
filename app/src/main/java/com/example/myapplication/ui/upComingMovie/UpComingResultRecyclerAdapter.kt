d6897d426fe3640953ecd190ab2b4422dcf6b74e
ts moosavi <tt.musavi@gmail.com>
2022-05-23 20:55:21 +0430

    complete UpComingResultRecyclerView

---
+++ app/src/main/java/com/example/myapplication/ui/upComingMovie/UpComingResultRecyclerAdapter.kt
@@ -0,0 +1,48 @@
+package com.example.myapplication.ui.upComingMovie
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.myapplication.R
+import com.example.myapplication.databinding.MovieListItemViewBinding
+import com.example.myapplication.databinding.UpComingListItemBinding
+import com.example.myapplication.model.Movie
+import com.example.myapplication.model.UpComingResult
+import com.example.myapplication.ui.search_result.SearchResultAdapter
+import com.example.myapplication.ui.search_result.SearchedMovieDiffCallback
+
+class UpComingResultRecyclerAdapter: ListAdapter<UpComingResult, UpComingResultRecyclerAdapter.ItemHolder>(UpcomingMovieDiffCallback) {
+
+    class ItemHolder(val binding: UpComingListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+    }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemHolder {
+        val binding: UpComingListItemBinding = DataBindingUtil.inflate(
+            LayoutInflater.from(parent.context),
+            R.layout.up_coming_list_item,
+            parent,false
+        )
+        return ItemHolder(binding)
+
+    }
+
+    override fun onBindViewHolder(holder: ItemHolder, position: Int) {
+        val movie = getItem(position)
+        holder.binding.movie = movie
+
+    }
+}
+    object UpcomingMovieDiffCallback : DiffUtil.ItemCallback<UpComingResult>() {
+
+        override fun areItemsTheSame(oldItem: UpComingResult, newItem: UpComingResult): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem:UpComingResult, newItem: UpComingResult): Boolean {
+            return oldItem.id == newItem.id
+        }
+}
--- app/src/main/java/com/example/myapplication/ui/upComingMovie/UpComingResultRecyclerView.kt
+++
@@ -1,4 +0,0 @@
-package com.example.myapplication.ui.upComingMovie
-
-class UpComingResultRecyclerView {
-}
