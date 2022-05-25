fcb9bba86460f9e1d6f10e03e00e727f60d0aed0
ts moosavi <tt.musavi@gmail.com>
2022-05-25 14:09:38 +0430

    add binding to ShowVideoFragment

---
+++ app/src/main/java/com/example/myapplication/ui/video_webview/ShowVideoFragment.kt
@@ -0,0 +1,31 @@
+package com.example.myapplication.ui.video_webview
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.R
+import com.example.myapplication.databinding.FragmentShowVideoBinding
+
+
+class ShowVideoFragment : Fragment() {
+    lateinit var binding:FragmentShowVideoBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_show_video,container,false)
+        return inflater.inflate(R.layout.fragment_show_video, container, false)
+    }
+
+
+}
